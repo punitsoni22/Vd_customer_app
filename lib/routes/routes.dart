8e02acb54e5f8b83203225caf464cd1d585e673c
Dhruv-git19 <dhruvprajapat016@gmail.com>
2025-08-20 20:58:28 +0530

    routes naming changes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,10 +1,10 @@
 class AppRoutes {
-  static const String otpscreen = '/otp';
-  static const String loginscreen = '/login';
-  static const String regionscreen = '/region';
-  static const String homeScreen = '/home';
-  static const String subscriptionScreen = '/subscription';
-  static const String productScreen = '/products';
-  static const String cartScreen = '/cart';
-  static const String profileScreen = '/profile';
+  static const String otpscreen = 'otp';
+  static const String loginscreen = 'login';
+  static const String regionscreen = 'region';
+  static const String homeScreen = 'home';
+  static const String subscriptionScreen = 'subscription';
+  static const String productScreen = 'products';
+  static const String cartScreen = 'cart';
+  static const String profileScreen = 'profile';
 }
