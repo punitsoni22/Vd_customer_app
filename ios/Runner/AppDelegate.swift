c116b37673e96de92b75d3ff126f91f8c5a3d308
Punit <punit@alliancecorps.com>
2025-12-01 22:14:53 +0530

    Adding secrets in gitIgnore

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      GMSServices.provideAPIKey("AIzaSyAYig-f-s8na0VmF_6S7we7n_XvMzBEfnI")
+      GMSServices.provideAPIKey(Secrets.googleMapsApiKey)
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
